9622bce01d2b33161c2ff5e218ce8cad07d56c11
Christian <1605942@brunel.ac.uk>
2018-07-03 11:31:54 +0100

    Implemented the random spawner for fruit.
    For this task I created a seperate script called randomspawner that instantiates the 4 fruits depending on a random choice. I also ensured that the lemons do not pop up frequently by adding a double chance check to see if it is successfully chosen to instantiate.

---
+++ Fruit Rush/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour 
+{
+    public GameObject banana, watermelon, orange, lemon;
+
+    public float spawnRate = 2f; //Spawn rate of objects
+    float nextSpawn = 0f;
+    float randomX, randomZ; //This needs to be set to max and minimum values of the game board
+    int chance; //Ensure there is a random chance that orange is chosen, because I do not want too many oranges showing up.
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Time.time > nextSpawn)
+        {
+            int whatToSpawn = Random.Range(1, 5);
+            randomX = Random.Range(-8, 8);
+            randomZ = Random.Range(-8, 8);
+            Vector3 randomPosition = new Vector3(randomX, 1, randomZ);
+
+            if(whatToSpawn == 3)//If the random value is 3, then go through another check to see if
+            {
+                chance = Random.Range(1, 4);
+                if(chance == 3)
+                {
+                    Debug.Log(whatToSpawn.ToString());
+                    InstantiateSpawn(chance, randomPosition); //Spawn the orange if it is a 3.
+                }
+            }
+            else
+            {
+                InstantiateSpawn(whatToSpawn, randomPosition);
+            }
+         }
+	 }
+
+    void InstantiateSpawn(int whatToSpawn, Vector3 randomPosition)
+    {
+        Debug.Log(whatToSpawn.ToString());
+
+        switch (whatToSpawn) //Switch statement that spawns depending no the number of the case.
+        {
+            case 1:
+                Instantiate(banana, randomPosition, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(watermelon, randomPosition, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(lemon, randomPosition, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(orange, randomPosition, Quaternion.identity);
+                break;
+        }
+        nextSpawn = Time.time + spawnRate;
+    }
+}
